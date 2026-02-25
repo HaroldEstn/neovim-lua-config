7cbcd830afc5ebb95459193513447ee5e9c3fa50
HaroldEstn <haroldestn@gmail.com>
2026-02-25 18:59:33 -0500

    fix lsp server error

--- lua/config.lua
+++ lua/config.lua
@@ -30,17 +30,18 @@ require('statusline')
 require("bufferline").setup{}
 
 -- lsp config
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+vim.lsp.enable('pyright')
+-- local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-local lspconfig = require('lspconfig')
+-- local lspconfig = require('lspconfig')
 
-local servers = { 'pyright' }
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    -- on_attach = my_custom_on_attach,
-    capabilities = capabilities,
-  }
-end
+-- local servers = { 'pyright' }
+-- for _, lsp in ipairs(servers) do
+--   lspconfig[lsp].setup {
+--     -- on_attach = my_custom_on_attach,
+--     capabilities = capabilities,
+--   }
+-- end
 
 local luasnip = require 'luasnip'
 
